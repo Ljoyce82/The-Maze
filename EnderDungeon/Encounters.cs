d3944ff7626a56f29ad880cb9b8aadffa62a87d8
Levi Joyce <linuxgamer82@gmail.com>
2024-05-06 12:06:01 -0400

    trying fix user input

--- EnderDungeon/Encounters.cs
+++ EnderDungeon/Encounters.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using EnderDungeon;
+using TheMaze;
 
-namespace EnderDungeon
+namespace TheMaze
 {
     public class Encounters
     {
--- EnderDungeon/Player.cs
+++ EnderDungeon/Player.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EnderDungeon
+namespace TheMaze
 {
     public class Player
     {
